c7a193e9dd5761b03d71da1a42f036e459b4564c
Roman <devmixlab@gmail.com>
2026-04-18 05:22:13 +0300

    fix

--- src/Card/card.tokens.ts
+++ src/Card/card.tokens.ts
@@ -1,4 +1,4 @@
-export const kinds = ['filled', 'solid', 'soft', 'outlined', 'flat', 'ghost'] as const;
+export const kinds = ['filled', 'solid', 'soft', 'outlined', 'ghost'] as const;
 export type Kind = (typeof kinds)[number];
 
 export const density = ['none', 'xs', 'sm', 'md', 'lg', 'xl'] as const;
